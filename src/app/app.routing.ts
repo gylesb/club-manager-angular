6eb82c1d331b7e23c73039a598a72cb15115219e
Gyles Batara <gyles.batara@live.com>
2017-12-18 10:56:46 -0800

    Add components to routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,12 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { WelcomeComponent } from './welcome/welcome.component'
-import {  AboutComponent } from './about/about.component'
+import { WelcomeComponent } from './welcome/welcome.component';
+import {  AboutComponent } from './about/about.component';
+import { AdminComponent } from './admin/admin.component';
+import { EditMembersComponent } from './edit-members/edit-members.component';
+import { ListMembersComponent } from './list-members/list-members.component';
+import { MemberProfileComponent } from './member-profile/member-profile.component';
+import { ViewUsersComponent } from './view-users/view-users.component';
 
 const appRoutes: Routes = [
   {
@@ -12,6 +17,26 @@ const appRoutes: Routes = [
     path: 'about',
     component: AboutComponent
   },
+  {
+    path: 'admin',
+    component: AdminComponent
+    },
+    {
+      path: 'edit-members',
+      component: EditMembersComponent
+    },
+    {
+    path: 'list-members',
+    component: ListMembersComponent
+    },
+    {
+      path: 'member-profile',
+      component: MemberProfileComponent
+    },
+    {
+    path: 'view-users',
+    component: ViewUsersComponent
+    }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
