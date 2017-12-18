389e6b8c43bb02357214225c9f9a60e1ad2c5a55
Gyles Batara <gyles.batara@live.com>
2017-12-18 13:49:43 -0800

    Initialize Firebase. Going to work on initializing itsome more

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyDHfg4z9WDxLVU4jzo_neqmHej4tZR8rm8",
+  authDomain: "club-manager-79d1d.firebaseapp.com",
+  databaseURL: "https://club-manager-79d1d.firebaseio.com",
+  storageBucket: "club-manager-79d1d.appspot.com",
+  messagingSenderId: "147954500254"
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -12,6 +15,13 @@ import { AdminComponent } from './admin/admin.component';
 import { EditMembersComponent } from './edit-members/edit-members.component';
 import { ListMembersComponent } from './list-members/list-members.component';
 import { MemberProfileComponent } from './member-profile/member-profile.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket,
+};
 
 @NgModule({
   declarations: [
@@ -28,7 +38,9 @@ import { MemberProfileComponent } from './member-profile/member-profile.componen
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
